31cb939cd7f22338e3133569b375950ed1193c7d
JC Gillet <jcgillet@users.noreply.github.com>
2014-10-24 17:06:34 -0400

    Update sayan.py

--- sayan.py
+++ sayan.py
@@ -1,8 +1,9 @@
 name = input('Nom: ')
-dict = {'a': "ka", 'b': "zu", 'c': "mi", 'd': "te", 'e': "ku", 'f': "lu", 'g': "ji", 'h': "ri", 'i': "go", 'j': "zus", 'k': "me", 'l': "ta", 'm': "rin", 'n': "to", 'o': "mo", 'p': "no", 'q': "ke", 'r': "shi", 's': "ari", 't': "chi", 'u': "do", 'v': "ru", 'w': "mei", 'x': "na ", 'y': "fu", 'z': "zi"}
+d = {'a': "ka", 'b': "zu", 'c': "mi", 'd': "te", 'e': "ku", 'f': "lu", 'g': "ji", 'h': "ri", 'i': "go", 'j': "zus", 'k': "me", 'l': "ta", 'm': "rin", 'n': "to", 'o': "mo", 'p': "no", 'q': "ke", 'r': "shi", 's': "ari", 't': "chi", 'u': "do", 'v': "ru", 'w': "mei", 'x': "na ", 'y': "fu", 'z': "zi"}
 
 s=""
 for c in name:
-    s+= dict[c]
+    if d.has_key(c):
+        s+= dict[c]
 
-print s
+print "Résultat: ",s
